4e60b752869ddcf8aac1bd90b57c29f5a2976efe
Anton <147809331+antonioarias1193@users.noreply.github.com>
2024-03-15 09:14:32 +0700

    Add files via upload

--- takaware_product/food_container/spesific_product_Food_Container/js_spesific_product_Food_Container/parallax_effect.js
+++ takaware_product/food_container/spesific_product_Food_Container/js_spesific_product_Food_Container/parallax_effect.js
@@ -26,4 +26,35 @@ document.addEventListener("DOMContentLoaded", function () {
 		parallaxContainer.style.display = positionfixed > 340 ? "none" : "block";
 		parallaxText.style.position = positionfixed > 340 ? "relative" : "fixed";
 	});
+
+	// =============================== Randomize Text Color =================================
+	function randomElementFromArray(array) {
+		const shuffledArray = shuffleArray(array); // Menggunakan fungsi shuffleArray sebelumnya
+		const randomIndex = Math.floor(Math.random() * shuffledArray.length);
+		return shuffledArray[randomIndex];
+	}
+
+	// Fungsi untuk mengacak array (Fisher-Yates shuffle)
+	function shuffleArray(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]]; // Menukar elemen
+		}
+		return array;
+	}
+
+	// Contoh penggunaan
+	const myArray = [
+		"rgb(74, 19, 213)",
+		"rgb(11, 223, 50)",
+		"rgb(235, 46, 96)",
+		"rgb(9, 197, 169)",
+		"rgb(254, 150, 4)",
+	];
+	const randomElement = randomElementFromArray(myArray);
+	console.log(randomElement); // Output: satu elemen acak dari array
+
+	const randomColor = document.querySelector(".parallax-text");
+	randomColor.style.color = randomElement;
+	// =============================== Randomize Text Color =================================
 });
